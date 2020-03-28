012dcc14c1e0ec6e2dcaac123b75a1a400e2d5d0
Samuel Berensohn <samberensohn@gmail.com>
2020-03-27 17:27:26 -0700

    EventReminderMain: Correct Template Files put in

--- api/EventReminderMain.java
+++ api/EventReminderMain.java
@@ -120,7 +120,7 @@ public class EventReminderMain {
     }
 
     public static void getEmailInfo() throws Exception{
-        File file = new File("email.txt");
+        File file = new File("emails.txt");
         BufferedReader br = new BufferedReader(new FileReader(file));
 
         username = br.readLine();
@@ -130,8 +130,8 @@ public class EventReminderMain {
 
     public static void getKeys() throws Exception{
 
-        File newsFile = new File("api" + File.separator + "newsKey.txt");
-        File eventFile = new File("api" + File.separator + "eventKey.txt");
+        File newsFile = new File("api" + File.separator + "NewsApiKey.txt");
+        File eventFile = new File("api" + File.separator + "EventApiKey.txt");
 
         BufferedReader newsBr = new BufferedReader(new FileReader(newsFile));
         BufferedReader eventBr = new BufferedReader(new FileReader(eventFile));
